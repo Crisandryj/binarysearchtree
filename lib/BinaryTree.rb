2e3f47b8d45461489880a3fefb408eff70980811
Crisandry Javier <crisandryj@gmail.com>
2022-05-27 09:57:47 -0400

    BinarayTree class added

---
+++ lib/BinaryTree.rb
@@ -0,0 +1,15 @@
+require_relative 'Node'
+
+class BinaryTree
+
+    def initialize(array = nil, root = nil)
+        @array = array
+        @root = root
+    end 
+
+
+    def build_tree
+        
+    end 
+
+end 
---
+++ lib/Node.rb
@@ -0,0 +1,12 @@
+class Node
+
+    attr_accessor :data, :left_chidren, :right_children
+
+    def initialize(data = nil, left_chidren = nil, right_children = nil)
+        @data = data
+        @left_chidren = left_chidren
+        @right_children = right_children
+    end 
+
+
+end 
