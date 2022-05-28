5ab15d082b416100d4ea638c2e2ed7b0da5617de
Crisandry Javier <crisandryj@gmail.com>
2022-05-28 09:26:30 -0400

    cleaned code

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -1,58 +1,54 @@
 require_relative 'Node'
 
 class BinaryTree
+  attr_accessor :array, :root
 
-    attr_accessor :array, :root
+  def initialize(array = nil)
+    @array = array
+    @root = build_tree(array)
+  end
 
-    def initialize(array = nil)
-        @array = array
-        @root = build_tree(array)
-    end 
+  # def pretty_print(node = @root, prefix = '', is_left = true)
+  #     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+  #     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+  #     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  # end
 
-    # def pretty_print(node = @root, prefix = '', is_left = true)
-    #     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    #     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    #     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-    # end
+  def build_tree(array)
+    sorted_array = array.sort.uniq
+    start = 0
+    last = sorted_array.length - 1
 
-    def build_tree(array)
-        sorted_array = array.sort.uniq
-        start = 0
-        last = sorted_array.length - 1
+    if start > last
+      nil
+    else
+      mid = sorted_array.length / 2
+      root = sorted_array[mid]
+      node = Node.new(root)
+      node.left = build_tree(sorted_array[0...mid - 1])
+      node.right = build_tree(sorted_array[mid + 1...last])
+      root
+    end
+  end
 
-        if start > last
-            return nil
-        else
-            mid = sorted_array.length/2
-            root = sorted_array[mid]
-            node = Node.new(root)
-            node.left = build_tree(sorted_array[0...mid-1])
-            node.right = build_tree(sorted_array[mid+1...last])
-            return root;
-        end
-    end 
+  def insert(value)
+    if current_node.nil?
+      current_node.data = value
+      return
+    end
+    # recursively
+    # compare value to the root
+    current_node = @root
+    current_node = if current_node < value
+                     # if less than root - go to left tree else go to right tree
+                     insert(current_node.left)
+                   else
+                     insert(current_node.right)
+                   end
+    # if value = root ...return (add nothing)
+    # insert when returns nil
+  end
+end
 
-    def insert(value)
-        #recursively
-        #compare value to the root 
-        current_node = @root
-        if current_node < value 
-        #if less than root - go to left tree else go to right tree
-        current_node = current_node.left
-        else
-            current_node = current_node.right
-        end 
-        if current_node = nil 
-            current_node.data = value
-        end 
-        #if value = root ...return (add nothing)
-        #insert when returns nil
-    end 
-
-end 
-
-
-array = [1,2,3,4,5,6,7]
+array = [1, 2, 3, 4, 5, 6, 7]
 new = BinaryTree.new(array)
-
-
--- lib/Node.rb
+++ lib/Node.rb
@@ -1,12 +1,9 @@
 class Node
+  attr_accessor :data, :left, :right
 
-    attr_accessor :data, :left, :right
-
-    def initialize(data = nil, left_chidren = nil, right_children = nil)
-        @data = data
-        @left = left_chidren
-        @right = right_children
-    end 
-
-
-end 
+  def initialize(data = nil, left_chidren = nil, right_children = nil)
+    @data = data
+    @left = left_chidren
+    @right = right_children
+  end
+end
