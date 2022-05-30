077ecba8b06c7416631af588823d4a12fc79f294
Crisandry Javier <crisandryj@gmail.com>
2022-05-29 20:32:32 -0400

    updated

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -8,11 +8,7 @@ class BinaryTree
     @root = build_tree(array)
   end
 
-  # def pretty_print(node = @root, prefix = '', is_left = true)
-  #     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  #     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-  #     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-  # end
+
 
   def build_tree(array)
     last = array.length - 1
