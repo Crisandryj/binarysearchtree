ec669b0427a0875ea3bf3d5df1a380ab7e5bda12
Crisandry Javier <crisandryj@gmail.com>
2022-05-31 15:57:39 -0400

    fresh start

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -11,56 +11,21 @@ class BinaryTree
 
 
   def build_tree(array)
-    last = array.length - 1
 
-    return nil if array.empty?
-    
-      mid = array.length / 2
-      root_node = Node.new(array[mid])
-      root_node.left = build_tree(array[0...mid - 1])
-      root_node.right = build_tree(array[mid + 1...last])
-      root_node
   end
 
 
 
-  def insert(value, node = root)
-    return nil if node.data == value
-    # recursively
-    # compare value to the root
-    if node.data < value
-      # if less than root - go to left tree else go to right tree
-      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
-    else
-      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
-    end
-    # if value = root ...return (add nothing)
-    # insert when returns nil
+  def insert()
+
+
   end
 
-  def delete(value, node = root)
-    return node.data = nil if node.data == value 
-
+  def delete()
     #delete leaf in the tree - previous node no longer points to it
-    #case 1
-    if node.data > value
-      node.left.nil? node.left.data = nil : delete(value, node.left)
-    else
-      node.right.nil? node.right.data = nil : delete(value, node.right)
-    end 
+    #case 1      node.right.nil? node.right.data = nil : delete(value, node.right)
     #case 2
     #one child node - replace it with its child (previous node points to the child)
-    if node.data > value
-      if node.left == value && node.right.nil?
-       node.left = node.left.left 
-      end 
-    else
-      if node.right == value && node.left.nil?
-        node.right = node.right.right
-      end 
-    end 
-
-
     #node with 2 childs - next biggest - find the left most node (becomes new key) (recursively remove)
   end 
 
