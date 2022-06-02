2a2a4e50b7d1cb9bc726e69d3f16a51cb1a14979
Crisandry Javier <crisandryj@gmail.com>
2022-06-02 17:06:42 -0400

    updated delete

--- lib/BinaryTree.rb
+++ lib/BinaryTree.rb
@@ -39,26 +39,43 @@ class BinaryTree
 
   def delete(value, node = root)
     # case 1 node.right.nil? node.right.data = nil : delete(value, node.right)
-    # delete leaf in the tree - previous node no longer points to it
-
-    return value if node == nil
+    # delete leaf in the tree - previous node no longer points to i
   
     if value < node.data
-      node.left == value ? node.left = nil : delete(value, node.left)
+      node.left.data == value ? node.left = nil : delete(value, node.left)
     else
-      node.right == value ? node.right = nil : delete(value, node.right)
+      node.right.data == value ? node.right = nil : delete(value, node.right)
     end
 
     
     # case 2
     # one child node - replace it with its child (previous node points to the child)
+    if value < node.data
+      if node.right.nil?
+      node.left.data == value ? node = node.left.data : delete(value, node.left)
+      end
+    else
+      if node.left.nil?
+      node.right.data == value ? node = node.right.data : delete(value, node.right)
+      end
+    end
+    if node.left.nil? || node.right.nil?
+    end
+
+
     # node with 2 childs - next biggest - find the left most node (becomes new key) (recursively remove)
   end
+
+
+  def left_most(node = root)
+
+  end 
 end
 
 array = [1, 2, 3, 4, 5, 6, 7]
 new = BinaryTree.new(array)
 
 new.insert(0)
-p new.delete(7)
+new.delete(1)
 new.pretty_print
+
